c95e196c26bdc52192210f3582b559abaa2eb28b
Jonny Burger <jonathanburger11@gmail.com>
2023-11-01 18:45:25 +0100

    main

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,4 +1,3 @@
-import {Audio, Sequence, staticFile} from 'remotion';
 import React from 'react';
 import {AbsoluteFill} from 'remotion';
 import {MainSequence} from './MainSequence';
@@ -6,9 +5,6 @@ import {MainSequence} from './MainSequence';
 export const Main: React.FC = () => {
 	return (
 		<AbsoluteFill>
-			<Sequence from={20}>
-				<Audio src={staticFile('idontwannawork.wav')} />
-			</Sequence>
 			<MainSequence />
 		</AbsoluteFill>
 	);
